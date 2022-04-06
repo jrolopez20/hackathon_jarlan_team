f8a77230374e8d8fb9d92d3f8f285d07d7e53361
Javier Rodríguez <jrolopez20@gmail.com>
2022-04-05 22:07:44 -0600

    Mockup the story

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.sidebar.write("""
 	# *Smart Water Management*
-	Project description goes here 
+	Describes the current situation of the water supply in Chihuahua and how to contribute to beat this problem
 	""")
 	
 st.sidebar.title('Interactive options:')
@@ -14,7 +14,24 @@ st.sidebar.write("""
 	*© 2022 [Universidad Autónoma de Chihuahua](https://uach.mx)*
 	""")
 
-st.title('TODO')
+"""# Actual situation"""
+col1, col2 = st.columns(2)
+
+with col1:
+	st.write('Some critical text goes here.')
+	st.write('Lorem ipsum dolor sit amet consectetur adipiscing elit viverra mauris, taciti dapibus nec id at dictumst montes sem, praesent proin lacinia senectus aliquam et malesuada diam. In iaculis sociosqu urna conubia habitasse nam, habitant id nec dis vehicula proin, tempus nascetur varius volutpat dignissim. Vulputate volutpat erat venenatis nam augue conubia maecenas, nostra tempus donec montes pellentesque tincidunt, justo morbi egestas senectus eleifend iaculis')
+
+with col2:
+	st.image('https://www.siliconrepublic.com/wp-content/uploads/2017/07/Leaking-pipe-718x523.jpg')
+
+"""# So what is the problem?"""
+"""## How to prevent water leaks efficiently"""
+
+"""# Solution"""
+"""With this work we offer a possible solution to efficiently mitigate and prevent all water leaks and contribute to its conservation."""
+
+
+"""TODO Here we will start displaying chart as part of our solution"""
 
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
