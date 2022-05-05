34dfaca6560ae4949207729775cfa26a51385d18
pykto <alanr15.lll@gmail.com>
2022-05-05 15:38:40 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -247,31 +247,31 @@ if st.button("""%s""" % translation['evaluate']):
     
     """%s""" % translation['error_msg']
     st.write(new_prediction)
-    st.markdown(
-        """
-        <style>
-        .container {
-            display: flex;
-        }
+    # st.markdown(
+    #     <style>
+    #     .container {
+    #         display: flex;
+    #     }
         
-        """%s""" % translation['error_msg']
+    #     """%s""" % translation['error_msg']
         
-        st.markdown(
-            """
-            <style>
-            .container {
-                display: flex;
-            }
+        # st.markdown(
             
-            .zone-active {
-                float: left;
-                position: absolute;
-                height: 100% !important;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
+        #     <style>
+        #     .container {
+        #         display: flex;
+        #     }
+            
+        #     .zone-active {
+        #         float: left;
+        #         position: absolute;
+        #         height: 100% !important;
+        #     }
+        #     </style>
+        #     """,
+        #     unsafe_allow_html=True
+        #     """
+        # )
 
     st.markdown(
         f"""
